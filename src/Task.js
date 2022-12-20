defbb34fb4c1edffb2a74c78d3949d6495959af4
Tulus Suryo Widodo <223200039@student.unaki.ac.id>
2022-12-20 13:48:03 +0700

    first

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://final-project-deployment-f90b9.web.app" // TODO: replace this
 const studentName = "Tulus" // TODO: replace this
 const studentId = "FE2711948" // TODO: replace this
 
